840e05d4ee2c94a7177e9d415f779b52c35a99c9
JsemiC <ghufran.alimam@gmail.com>
2020-07-18 09:44:03 +0300

    adding webpack

--- app/server/server.js
+++ app/server/server.js
@@ -2,7 +2,13 @@
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
-
+const dotenv = require("dotenv");
+var AYLIENTextAPI = require('aylien_textapi');
+dotenv.config();
+var textapi = new AYLIENTextAPI({
+  application_id: process.env.API_ID,
+  application_key: process.env.API_KEY
+});
 const app = express()
 
 app.use(express.static('dist'))
