4fed5fcdd819c694d9785e52501317f10a7d15d1
JsemiC <ghufran.alimam@gmail.com>
2020-07-18 10:52:01 +0300

    Final webpack

--- app/client/index.js
+++ app/client/index.js
@@ -2,7 +2,7 @@
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
 
-console.log(checkForName);
+// console.log(checkForName);
 
-alert("I EXIST")
-console.log("CHANGE!!");
+// alert("I EXIST")
+// console.log("CHANGE!!");
--- app/server/server.js
+++ app/server/server.js
@@ -3,6 +3,7 @@ var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
 const dotenv = require("dotenv");
+const bodyParser = require("body-parser")
 var AYLIENTextAPI = require('aylien_textapi');
 dotenv.config();
 var textapi = new AYLIENTextAPI({
@@ -16,8 +17,8 @@ app.use(express.static('dist'))
 console.log(__dirname)
 
 app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+    res.sendFile('dist/index.html')
+    // res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
 // designates what port the app will listen to for incoming requests
